8aab7a3483c19ed3aab3582987e321aa9a69b9a3
unknown <17078.reza@gmail.com>
2022-03-22 10:49:15 +0700

    first commit

---
+++ file.py
@@ -0,0 +1,9 @@
+#ini buat coba aja 
+print("Selamat datang di ICL")
+print("[1] Login")
+print("[2] Pengabdian Masyarakat")
+print("[3] Praktikum")
+print("[4] Blog")
+print("[5] Daftar Asisten")
+print("[6] About Us")
+print("[7] Keluar")
